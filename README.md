c6db603dfec7ead12a8a4ad2d98145722c7af0cc
Jaidip <jaidip1994@gmail.com>
2021-05-06 18:16:30 +0530

    Files Changed

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Cowin Vaccination Slot Tracker
- 
+
+### Problem Statement
+In covid situation its a very crucial thing to get vaccinated and the most difficult part is to get a vaccination slot. As currently there is a medical supply shortage so as a result the slots available are very less. 
+
+### How to get slots for vaccination ?
+Answer is via automation. So that was my Idea, I know cowin open APIs are present but still its been a while i have played around with automation Tools, for this use case selenium, Once there is a slot available notification is being sent to the user, via windows10 notification, in whatsapp and in Andoid app ( via PushBullet APP)
+
+### Prerequisite 
+1. This script is written only for Windows based OS , especially windows 10.
+2. Python 3.5 > 
+3. Need to download PushBullet App https://www.pushbullet.com/, on your Android and Windows machine, so using this one type of notification will sent, and it will be synced across your configured devices
+4. After downloading the pushbullet app, you need to get the access token.
+Got to https://www.pushbullet.com/#settings -> Settings -> Account -> Access Token -> Create Access Token. Copy that and keep it aside.
+5. This only looks for Paid Vaccination Center , as this was my ask. Script can be enhanced by changing the xpath of the paid to any. 
+6. So as to run the script run this command, after cloning the project.
+    ```shell
+    $ pip install -r requirement.txt
+    ```
+7. Then in the open `cowinTracker.py` and then provide 
+    -   list of Pincode 
+    -   access token created in step 5
+    -   Whatsapp Group or Person's Name
+    
+    ![pinv](Cowin-Vaccination-Slot-Tracker\utility\carbon.png)
+8. Now run `cowinTracker.py` this will run infinetely for each Pincode & Paid Vaccination Center and it will identify whether any vacination slots are available or not after every 10mins interval
+    ```shell
+    $ python cowinTracker.py
+    Trying for 1 Times
+    |████████████████████████████████████████| 7/7 [100%] in 27.7s (0.25/s)
+    Will Try after 10mins...
+    [22164:24836:0506/164442.362:ERROR:gpu_init.cc(426)] Passthrough is not supported, GL is disabled
+    Trying for 2 Times
+    |████████████████████████████████████████| 7/7 [100%] in 27.7s (0.25/s)
+    Will Try after 10mins...
+    Trying for 3 Times
+    |████████████████████████████████████████| 7/7 [100%] in 28.0s (0.25/s)
+    Will Try after 10mins... 
+    ```
+9. In Pushbullet App it will be sent like this 
+    ![PushBulletApp](Cowin-Vaccination-Slot-Tracker\utility\pushBullet.jpg)
+10. And in windows Notification Tab
+    ![Windows Notification](Cowin-Vaccination-Slot-Tracker\utility\windowsToast.jpg)
---
+++ cowinTracker.py
@@ -0,0 +1,108 @@
+# https://www.pushbullet.com/#people
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import datetime
+from win10toast import ToastNotifier
+import requests
+import json
+import pprint
+from alive_progress import alive_bar
+
+PINCODE_LIST = ["560066", "560020", "560030", "560011", "560078", "560017", "560084" ]
+TOKEN = 'o.maWrkwmNzKLIoN0AgsChj95xdtiMXX3G'
+WHATSGRPORPERSONNAME = 'Sashu'
+
+def pushbullet_message(body):
+    msg = { "type" : "note", "title" : "Available Slots", "body": body}
+    resp = requests.post('https://api.pushbullet.com/v2/pushes', 
+                        data=json.dumps(msg),
+                        headers={'Authorization': 'Bearer ' + TOKEN,
+                                'Content-Type': 'application/json'})
+    if resp.status_code != 200:
+        raise Exception('Error',resp.status_code)
+    else:
+        print ('Message sent')
+
+def messageParse(dict_val):
+    final_message = ""
+    for key, value in dict_val.items():
+        final_message += f'Hospital Name is {key} \n Pincode: {value["pincode"]} \n Availaibility on \n'
+        for date, availability in value["slots"].items():
+            # TODO :Tweak here
+            if date != '' and availability[0] not in ['Booked', 'NA']:
+                final_message += f'\t {date} -> No. {availability[0]}, Age {availability[1]} \n'
+        final_message += f'\n'
+    return final_message
+count = 1
+driver = webdriver.Chrome("utility\chromedriver.exe")
+driver.get("https://web.whatsapp.com/")
+time.sleep(10)
+while True:
+    print(f"Trying for {count} Times")
+    driver.get("https://www.cowin.gov.in/home")
+    driver.maximize_window()
+    availableHospital = {}
+    with alive_bar(len(PINCODE_LIST)) as bar:
+        for indpincode in PINCODE_LIST:
+            bar()
+            driver.find_element_by_xpath("//input[contains(@value, 18)]").click()
+            pincode = driver.find_element_by_xpath("//input[@data-placeholder='Enter your PIN']")
+            pincode.clear()
+            pincode.send_keys(indpincode)
+            pincode.send_keys(Keys.ENTER)
+            time.sleep(3)
+
+            datedetails = driver.find_elements_by_xpath("//slide//p")
+            datelist = [datedetail.text for datedetail in datedetails]
+
+            hospital_names = driver.find_elements_by_xpath("//span[contains(@class, 'paid-button')]/..")
+            hospital_names_list = [hospitalname.text for hospitalname in hospital_names]
+            for index, hospital_name in enumerate(hospital_names_list):
+                # get just paid
+                value = driver.find_elements_by_xpath(f"(//span[contains(@class, 'paid-button')])[{index+1}]/../../../../div[last()]//li//div[contains(@class,'slots-box')]//a")    
+                # get all
+                # value = driver.find_elements_by_xpath(f"(//h5[contains(@class, 'center-name')])[{index+1}]/../../../div[last()]//li//div[contains(@class,'slots-box')]//a")    
+                valueToConsider = False
+                countList = []
+                agebracket = []
+                for linkindex, link in enumerate(value):
+                    # TODO :Tweak here
+                    if link.text.isdigit():
+                        valueToConsider = True
+                    if link.text != 'NA':    
+                        age = driver.find_element_by_xpath(f"((//span[contains(@class, 'paid-button')])[{index+1}]/../../../../div[last()]//li//div[contains(@class,'slots-box')]//a)[{linkindex + 1}]/../div/span")    
+                    else: 
+                        age = None
+                    countList.append(link.text)
+                    agebracket.append(age.text if age is not None else age)     
+                if not valueToConsider:
+                    continue
+                availableHospital[hospital_name] = { 'slots' : dict(zip(datelist, zip(countList, agebracket))) }
+                availableHospital[hospital_name].update({"pincode" : indpincode}) 
+    
+
+    if bool(availableHospital):
+        today = datetime.datetime.now()
+        hr = ToastNotifier()
+        hr.show_toast("Hospital Slots Available", ", ".join(list(availableHospital.keys())))
+        pushbullet_message(messageParse(availableHospital))
+        # pywhatkit.sendwhatmsg('+918777621918', messageParse(availableHospital), datetime.datetime.now().hour, datetime.datetime.now().minute + 2 )
+        driver.get("https://web.whatsapp.com/")
+        time.sleep(15)
+        driver.find_element_by_xpath(f'//span[@title=\'{WHATSGRPORPERSONNAME}\']/../../..').click()
+        inp_xpath = "//footer/..//div[@contenteditable='true']"
+        input_box = driver.find_element_by_xpath(inp_xpath)
+        input_box.send_keys("Hospital Slots Available ", ", ".join(list(availableHospital.keys())))
+        input_box.send_keys(Keys.ENTER)
+    time.sleep(5)            
+
+    # pprint.pprint(availableHospital)
+    print("Will Try after 10mins...")
+    time.sleep(600)
+    count+=1
+
